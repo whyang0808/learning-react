6c8269aa57414523b211a76465307368898f8886
Yang Wei Hao <whyang0808@gmail.com>
2018-07-01 19:39:43 +0800

    Adding radium for media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      { id:'1', name: 'Max', age: '28' },
-      { id:'2', name: 'Manu', age: '29' },
-      { id:'3', name: 'Stephanie', age: '26' }
+      { id: '1', name: 'Max', age: '28' },
+      { id: '2', name: 'Manu', age: '29' },
+      { id: '3', name: 'Stephanie', age: '26' }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -98,14 +98,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>Toggle Person</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonHandler}>Toggle Person</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
