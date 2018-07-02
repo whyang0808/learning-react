c87dd4b5929827ebee150c80db39f67999655216
Yang Wei Hao <whyang0808@gmail.com>
2018-07-03 02:05:02 +0800

    A different approach to HOCs

--- react-complete-guide/src/hoc/WithClass.js
+++ react-complete-guide/src/hoc/WithClass.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const withClass = (props) => {
-    return (
-        <div className={props.classes}>
-            {props.children}
+const withClass = (WrappedComponent, className) => {
+    return (props) => (
+        <div className={className}>
+            <WrappedComponent/>
         </div>
     )
 }
